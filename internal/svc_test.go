0edf1ef54fbfca3ace8055628e8b3aed22678ad2
Jan K <janko@umich.edu>
2019-12-24 21:44:06 +0100

    add some test (#3)

---
+++ internal/svc_test.go
@@ -0,0 +1,43 @@
+package internal
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	tests := []struct {
+		name         string
+
+		givenUrl string
+		givenConc int
+		givenPayload string
+		givenMethod string
+
+		wantNil bool
+	}{
+		{
+			name: "should return not nil",
+
+			givenUrl: "www.google.com",
+			givenConc: 5,
+			givenMethod: "get",
+
+			wantNil: false,
+		},
+	}
+
+	required := require.New(t)
+
+	for _, tt := range tests {
+		tc := tt
+		t.Run(tc.name, func(t *testing.T) {
+			svc := NewSvc(tc.givenUrl, tc.givenConc, tc.givenMethod, tc.givenPayload)
+
+			required.NotNil(svc)
+			required.Equal( "", svc.payload)
+			required.Equal( 5, svc.concurrent)
+			required.Equal( "get", svc.method)
+		})
+	}
+}
